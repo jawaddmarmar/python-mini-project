f88f1d098c994089c3620eda9d439f22dff10ad5
Jawadmarmar <marmarjawad6@gmail.com>
2025-12-17 15:24:23 +0200

    adjusting some details and adding special codition

--- main.py
+++ main.py
@@ -3,21 +3,32 @@ password = input("enter your password:")
 def pwd_check(psd):
     has_number = False
     has_uppercase = False
-
+    has_special=False
+    specials="!@#$%^&*?"
     if len(psd) >=8:
 
         for x in psd:
             if x.isdigit():
                 has_number=True
-                             
+
+################################
+                          
             if x.isupper():
                 has_uppercase=True
-        if has_number and has_uppercase:
-           print("strong")    
-        elif has_number or has_uppercase:   
-            print("medium") 
-        else:
-            print("weak")
+
+###############################                
+
+            if x in specials:
+                has_special=True
+
+###############################                
+            if has_number and has_uppercase and has_special:
+               print("strong")    
+            elif has_number or has_uppercase or has_special:   
+                print("medium") 
+            else:
+                print("weak")
+            
     else:
         print("Your password is weak")
 
